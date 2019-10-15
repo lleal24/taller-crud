01ef93a89c6f20dada17558f44f507666cc92c94
lleal24 <lorenaleal24@gmail.com>
2019-10-14 18:03:03 -0700

    Creacion de modelo estrenos

---
+++ modelo/estrenosModelo.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const mongoose = require('mongoose'); //llamado del modulo mongoose
+
+const Schema = mongoose.Schema; //creador de plantilla de modelo mongoose
+
+const estrenosSchema = new Schema({
+    nombre: {
+        type:String, required: true
+    },
+    director:{
+        type: String
+    },
+    año:{
+        type:Number, default:0 
+    },
+    clasificacion:{
+        type:String, enum: ['A','AA','B','B15', 'C', 'D'] 
+    },
+    genero:{
+        type:Array
+    },
+	protagonistas:{
+		type:Array
+	},
+	pais:{
+		type: String
+	},
+	duracion:{
+		type:Number, default:30
+	}
+
+});
+
+//importacion del esquema
+module.exports = mongoose.model('estrenos', estrenosSchema)
