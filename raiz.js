bbb0068da7d52e4afd49d4c219a6d81868b37005
lleal24 <lorenaleal24@gmail.com>
2019-10-16 15:20:48 -0500

    Se agrego la ruta correspondiente a la categora nacionales

--- raiz.js
+++ raiz.js
@@ -7,8 +7,9 @@ const express = require('express'),
     infantiles = require('./rutas/rutaInfantiles'),
     clasicas = require('./rutas/rutaClasicas'),
     recomendadas = require('./rutas/rutaRecomendadas'),
+    nacionales = require('./rutas/rutaNacionales'),
 
-    app = express()  /* ,
+    app = express()   ,
     PORT = 3000,
     BD = 'peliculas'
 
@@ -17,21 +18,21 @@ mongoose.connect('mongodb://localhost:27017', {
     useUnifiedTopology: true,
     useNewUrlParser: true,
 }).then(() => {
-    console.log(`Pudimos ingresar a la base de datos <<${BD}>> con exito!!`.bgGreen)
+    console.log(`Pudimos ingresar a la base de datos <<${BD}>> con exito!!`)
 }).catch((err) => {
-    console.log(`No hay conexion:${err}`.bgRed)
-})  */
+    console.log(`No hay conexion:${err}`)
+})  
 
 
 // ----- CONEXION A LA BD LINUX ------------------------------
 //Observacion: para esta version es necesario pasarle esos parametros useNewUrlParser y useUnifiedTopology
-mongoose.connect("mongodb://localhost:27017/peliculas", { useNewUrlParser: true, useUnifiedTopology: true }) //conexion si no esta la db mongo la crea
+/* mongoose.connect("mongodb://localhost:27017/peliculas", { useNewUrlParser: true, useUnifiedTopology: true }) //conexion si no esta la db mongo la crea
     // ------ PROMESAS -------------------------------------
     .then(db => console.log('DB connected')) //Si se conecta
     .catch(err => console.log(err)); //Si falla la conexion
 
     app.set('port', process.env.PORT || 3030)
-
+ */
 
 // ------------CONFIGURACION PARA POSTMAN DE RUTAS-------------------------------------------
 app.use(bodyParser.json()); 
@@ -39,6 +40,7 @@ app.use('/api/estrenos', estrenos);
 app.use('/api/infantiles', infantiles);
 app.use('/api/clasicas', clasicas); 
 app.use('/api/recomendadas', recomendadas);
+app.use('/api/nacionales', nacionales);
 //----------------------------------------------------------------------------------
 
 
@@ -47,11 +49,11 @@ app.use((err, req, res, next) => {
 });
 
 //starting the server on LINUX
-app.listen(app.get('port'), () => { //app.listen(puerto , ()=>{Mensaje a mostrar})
+/* app.listen(app.get('port'), () => { //app.listen(puerto , ()=>{Mensaje a mostrar})
     console.log(`Server on port ${app.get('port')}`)
-});
-
-/* 
+}); */
+//----------------------------------------------------------------------------------
+ 
 app.listen(PORT, () => {
-    console.log(`Cuidado estamos usando el Puerto ${PORT}`.bgBlue)
-})  */
+    console.log(`Cuidado estamos usando el Puerto ${PORT}`)
+})  
